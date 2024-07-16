b11a7a709eea5cf017c957fa323a1680a8387e0d
Carlos <c.gacitua@duocuc.cl>
2024-07-16 00:31:25 -0400

    [200~[200~Se creo la carpeta 'modelo_entrenamiento' donde se adjunto un archivo llamado 'entrenar_modelo.py' el cual servira para entrenar el modelo de 'Predecir Kills'.
    Dento de la carpeta 'modelo_entrenamiento' se agrego la base de datos de counter strike.
    
    Al ejecutar el archivo 'entrenar_modelo.py' se creo un modelo llamado 'modelo_entrenado.pkl' que servira para predecir kills.
    
    Se creo la pagina 'predecir_kill.html' donde el usuario podra rellenar los campos que piden y la pagina le entregara un resultado segun la prediccion del modelo entrenado.
    
    Se modificaron las rutas de urls.py de la app 'paginas'.
    
    Solo se añadio el link a 'predecir_kill' en el index de la pagina.
    
    ACLARACION:
    
    Estos avances solo serviran para testear, se planea dejar la pagina final de una sola pagina que entregue diferentes modelos a eleccion del usuario, por ahora no es importante el diseño o la cantidad de modelos, lo ideal es seguir probando mas modelos con diferentes elecciones.~~

--- counter_strike/paginas/views.py
+++ counter_strike/paginas/views.py
@@ -1,6 +1,61 @@
+import joblib
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+# Cargar el modelo entrenado
+# Quitar "#" cuando se quiera entrenar el modelo
+# model = joblib.load('paginas/modelo_entrenamiento/modelo_entrenado.pkl')
+
+# Funcion para predecir las kills de la partida del jugador
+def predecir_kill(request):
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        equipo = request.POST.get('Equipo')
+        match_id = request.POST.get('MatchId')
+        round_id = request.POST.get('RoundId')
+        lethal_grenades = request.POST.get('RLethalGrenadesThrown')
+        non_lethal_grenades = request.POST.get('RNonLethalGrenadesThrown')
+        primary_assault_rifle = request.POST.get('PrimaryAssaultRifle')
+        primary_sniper_rifle = request.POST.get('PrimarySniperRifle')
+        primary_heavy = request.POST.get('PrimaryHeavy')
+        primary_smg = request.POST.get('PrimarySMG')
+        primary_pistol = request.POST.get('PrimaryPistol')
+        round_kills = request.POST.get('RoundKills')
+        round_assists = request.POST.get('RoundAssists')
+        round_headshots = request.POST.get('RoundHeadshots')
+        round_flank_kills = request.POST.get('RoundFlankKills')
+        starting_equipment_value = request.POST.get('RoundStartingEquipmentValue')
+        team_starting_equipment_value = request.POST.get('TeamStartingEquipmentValue')
+
+        # Convertir las características a float
+        features = [
+            float(equipo),
+            float(match_id),
+            float(round_id),
+            float(lethal_grenades),
+            float(non_lethal_grenades),
+            float(primary_assault_rifle),
+            float(primary_sniper_rifle),
+            float(primary_heavy),
+            float(primary_smg),
+            float(primary_pistol),
+            float(round_kills),
+            float(round_assists),
+            float(round_headshots),
+            float(round_flank_kills),
+            float(starting_equipment_value),
+            float(team_starting_equipment_value)
+        ]
+
+        # Hacer la predicción
+        prediction = model.predict([features])
+
+        # Retornar los resultados en el render
+        return render(request, 'predecir_kill.html', {'prediction': prediction[0]})
+
+    return render(request, 'predecir_kill.html')
 
 def index(request):
     return render(request, 'index.html')
